ae9c45011d00931711395073c30ff40a5c9be290
andkinnamon <dr.kinnamon@gmail.com>
2025-02-25 01:08:32 -0500

    1.0.4
    - Minor adjustments to answers
    - Adjustments to footer

--- script.js
+++ script.js
@@ -3,24 +3,24 @@ const questions = {
         { question: "List the Pauline Epistles in Chronological order.", answer: "Pauline Epistles (Chronological):<ul><li>Galatians</li><li>1 & 2 Thessalonians</li><li>1 & 2 Corinthians</li><li>Romans</li><li>Ephesians, Philippians, Colossians, Philemon</li><li>1 Timothy, Titus</li><li>2 Timothy</li></ul>" }
     ],
     lecture3a: [
-        { question: "Cara's generic Pauline letter outline.", answer: "Pauline Generic Outline:<ul><li>Opening — X to Y, grace and peace</li><li>Thanksgiving</li><li>Body — Doctrinal then Hortatory</li><li>Closing — peace-benediction, greetings, grace-benediction</li></ul>" }
+        { question: "Cara's generic Pauline letter outline.", answer: "Pauline Generic Outline:<ul><li>Opening – X to Y, grace and peace</li><li>Thanksgiving</li><li>Body – Doctrinal then Hortatory</li><li>Closing – peace-benediction, greetings, grace-benediction</li></ul>" }
     ],
     lecture4: [
         { question: "Outline of Galatians.", answer: "Galatians:<ul><li>1:1-5 – Opening</li><li>1:6-2:21 – Apostolic Defense<ul><li>2:15-21 is transition section</li></ul></li><li>3:1-5:12 – Justificaton by Faith</li><li>5:13-6:10 – Live by Spirit not Flesh</li><li>6:11-18 – Closing</li></ul>" },
-        { question: "Identify the chapter: “If anyone preaches any other gospel... let him be cursed.”", answer: "Galatians 1(:9)" }
+        { question: "Identify the chapter: “If anyone preaches any other gospel... let him be cursed.”", answer: "Galatians 1" }
     ],
     lecture5: [
-        { question: "Identify the chapter: “There is neither Jew nor Greek… for you are all one in Christ Jesus.”", answer: "Galatians 3(:28)" }
+        { question: "Identify the chapter: “There is neither Jew nor Greek… for you are all one in Christ Jesus.”", answer: "Galatians 3" }
     ],
     lecture6: [
-        { question: "Identify the chapter: “The fruit of the Spirit is love, joy, …”", answer: "Galatians 5(:22-23)" }
+        { question: "Identify the chapter: “The fruit of the Spirit is love, joy, …”", answer: "Galatians 5" }
     ],
     lecture7: [
-        { question: "Outline of 1 Thessalonians.", answer: "1 Thessalonians:<ul><li>1:1 — Opening</li><li>1:2-10 — Thanksgiving</li><li>2:1-3:13 — Defense, Thanksgiving, Tim’s Mission</li><li>4:1-5:22 — Hortatory</li><li>5:23-28 — Closing</li></ul>" }
+        { question: "Outline of 1 Thessalonians.", answer: "1 Thessalonians:<ul><li>1:1 – Opening</li><li>1:2-10 – Thanksgiving</li><li>2:1-3:13 – Defense, Thanksgiving, Tim’s Mission</li><li>4:1-5:22 – Hortatory</li><li>5:23-28 – Closing</li></ul>" }
     ],
     lecture8: [
-        { question: "Identify the chapter, “You accepted it not as the word of men, but as it truly is, the word of God.”", answer: "1 Thessalonians 2(:13)" },
-        { question: "Identify the chapter, “We will be caught up together with them in the clouds to meet the Lord.”", answer: "1 Thessalonians 4(:17)" }
+        { question: "Identify the chapter, “You accepted it not as the word of men, but as it truly is, the word of God.”", answer: "1 Thessalonians 2" },
+        { question: "Identify the chapter, “We will be caught up together with them in the clouds to meet the Lord.”", answer: "1 Thessalonians 4" }
     ],
     lecture9: [
         { question: "Outline of 2 Thessalonians.", answer: "2 Thessalonians:<ul><li>1:1-2 – Opening</li><li>2:1-12 – Man of Lawlessness</li><li>2:13-3:5 – Thanksgiving and Prayer Request</li><li>3:6-15 – Idleness</li><li>3:16-18 – Closing</li><li>1:3-12 – Thanksgiving</li></ul>" }
